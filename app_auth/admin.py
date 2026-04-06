2045348af80b4d473ba22d68e281c917ded863e1
dhm-blue-phoenix <blue.dhm.dev@gmail.com>
2026-04-06 20:04:51 +0200

    The `admin.py` file in `app_auth` has been updated

--- app_auth/admin.py
+++ app_auth/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Unregister the default User admin
+admin.site.unregister(User)
+
+# Re-register User with customized clear dashboard UI
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'date_joined')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_joined')
+    ordering = ('-date_joined',)
